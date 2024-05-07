63d96aedde8f9e515cf857f713761e25545265ad
Yacith-D <martinezyacith@gmail.com>
2024-05-07 15:35:57 -0600

    realizando funciones en el rejistrar

--- signup.js
+++ signup.js
@@ -1,10 +1,22 @@
 const singupform = document.querySelector("#singupform")
 singupform.addEventListener('submit', (e)=>{ 
-
-
-
-
-})
+    e.preventDefault()
+     const name = document.querySelector('name')
+     const email = document.querySelector('email') 
+     const password = document.querySelector('password')
+    
+     const Users = JSON.parse(localStorage.getItem('users')) || []
+     const isUserRegistered = Users.find(user => user.email === email)
+     if(isUserRegistered){
+         return alert('El usuario ya esta registado!')
+     }
+ 
+     Users.push({name: name, email: email, password: password})
+     localStorage.setItem('users', JSON.stringify(Users))
+     alert('Registro Exitoso!')
+     window.location.href = 'login.html'
+ 
+ })
 
 
 
